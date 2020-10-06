10429aebd07a7ebae74b79184485f157694b270e
Cemal Cetin <ascetincemal@gmail.com>
2020-10-06 17:41:46 +0200

    initial commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello class EU3");
+        // more changes are comings
+        // I am ready to commit this...
+
     }
 }
